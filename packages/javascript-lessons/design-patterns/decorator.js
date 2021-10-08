36dd4f1bc63df02a662074a6546f53c6da01a113
Roberto acuña <robertoacunapolo@gmail.com>
2021-10-08 09:16:17 -0500

    feat: Added the observable pattern example

--- packages/javascript-lessons/design-patterns/decorator.js
+++ packages/javascript-lessons/design-patterns/decorator.js
@@ -1,1 +1,47 @@
 // Implements the decorator pattern with examples
+/** 
+ * Decorators are a structural design pattern that aim to promote code reuse.
+   Similar to Mixins, they can be considered another viable alternative 
+   to object subclassing.
+   @see: https://www.oreilly.com/library/view/learning-javascript-design/9781449334840/ch09s14.html
+*/
+function MacBook() {
+  this.cost = function () {
+    return 997;
+  };
+  this.screenSize = function () {
+    return 11.6;
+  };
+}
+
+// Decorator 1
+function Memory(macbook = new MacBook()) {
+  const value = macbook.cost();
+  macbook.cost = function () {
+    return value + 75;
+  };
+}
+
+// Decorator 2
+function Engraving(macbook = new MacBook()) {
+  const value = macbook.cost();
+  macbook.cost = function () {
+    return value + 200;
+  };
+}
+
+// Decorator 3
+function Insurance(macbook = new MacBook()) {
+  const value = macbook.cost();
+  macbook.cost = function () {
+    return value + 250;
+  };
+}
+
+const macbookM1 = new MacBook();
+Memory(macbookM1);
+Engraving(macbookM1);
+Insurance(macbookM1);
+
+console.log(macbookM1.cost()); // 1222
+console.log(macbookM1.screenSize()); // 11.6
--- packages/javascript-lessons/design-patterns/observable.js
+++ packages/javascript-lessons/design-patterns/observable.js
@@ -1,2 +1,38 @@
 // Implements the observable pattern with a simple event emitter (X2 examples)
 // Use rxjs-observable-patterns to implement the observable pattern with rxjs
+
+function Click() {
+  this.handlers = [];
+}
+
+Click.prototype = {
+  on: function (handler) {
+    this.handlers.push(handler);
+  },
+  unsubscribe: function (handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  },
+  emit: function (data, thisArg) {
+    const scope = thisArg || globalThis;
+    this.handlers.forEach(function (handler) {
+      handler.call(scope, data);
+    });
+  },
+};
+
+function run() {
+  const click = new Click();
+
+  const handler = function (data) {
+    console.log("handler", data);
+  };
+
+  click.on(handler);
+  click.emit("click #1");
+  click.unsubscribe(handler);
+  click.emit("click #2");
+  click.on(handler);
+  click.emit("click #3");
+}
+
+run();
